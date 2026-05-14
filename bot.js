bb2184bb5a377c8e4de4d5610810c4bf5fc9b1c5
Lalit <lalit979932@gmail.com>
2026-05-15 00:20:47 +0530

    rgvb

--- bot.js
+++ bot.js
@@ -1,8 +1,6 @@
-
-
 const TelegramBot = require("node-telegram-bot-api");
 
-const token = "8786488490:AAHPs3nj54b2tk4jAzusYf0kgMydrgdtd74";
+const token = "8786488490:AAFm-5zyvCnu2LCmOvfoHWw_Nn2OsDXKMYI";
 
 const bot = new TelegramBot(token, {
   polling: true,
@@ -14,14 +12,14 @@ const movies = [
     title: "Vaazha Movie",
     slug: "vaazha-biopic-of-a-billion-boys-2025-youth-comedy-drama-full-movie-hd",
     downloadUrl: "https://t.me/onemoviedownloa/75",
-    poster: "https://cdn.shopify.com/...",
+    poster:
+      "https://cdn.shopify.com/s/files/1/0649/9945/6854/files/Vaazha_-_Biopic_of_a_Billion_Boys.png?v=1778779915",
     rating: 8,
     duration: "2h 22m",
   },
 ];
 
-
-// 👇 YE YAHAN ADD KARO
+// Normal /start
 bot.onText(/\/start$/, (msg) => {
   bot.sendMessage(
     msg.chat.id,
@@ -29,14 +27,14 @@ bot.onText(/\/start$/, (msg) => {
   );
 });
 
-
+// Website se aane wala slug
 bot.onText(/\/start (.+)/, async (msg, match) => {
-
-  console.log(match[1]); // ADD THIS
 
   const chatId = msg.chat.id;
 
   const slug = match[1];
+
+  console.log("Received slug:", slug);
 
   const movie = movies.find(
     (m) => m.slug === slug
@@ -44,26 +42,6 @@ bot.onText(/\/start (.+)/, async (msg, match) => {
 
   if (!movie) {
     return bot.sendMessage(chatId, "Movie not found");
-  }
-
-});
-
-// 👇 Iske neeche already wala code rahega
-bot.onText(/\/start (.+)/, async (msg, match) => {
-
-  const chatId = msg.chat.id;
-
-  const slug = match[1];
-
-  const movie = movies.find(
-    (m) => m.slug === slug
-  );
-
-  if (!movie) {
-    return bot.sendMessage(
-      chatId,
-      "Movie not found"
-    );
   }
 
   await bot.sendPhoto(
