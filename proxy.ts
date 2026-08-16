6e15800053517c2b1512a325b4541d0e0237d19f
Lalit <lalit979932@gmail.com>
2026-08-17 02:17:17 +0530

    f

--- proxy.ts
+++ proxy.ts
@@ -1,12 +1,12 @@
 import type { NextRequest } from "next/server";
-import { updateSession } from "@/lib/supabase/proxy";
+import { NextResponse } from "next/server";
+// import { updateSession } from "@/lib/supabase/proxy";
 
 export async function proxy(request: NextRequest) {
-  return updateSession(request);
+  // return updateSession(request);
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|apk)$).*)",
-  ],
+  matcher: [],
 };
