c9f6fa5b4a0ed978113385b745ecf0cc09842e0d
Lalit <lalit979932@gmail.com>
2026-09-17 02:59:13 +0530

    update dependencies lockfile

--- app/bollywood/page.tsx
+++ app/bollywood/page.tsx
@@ -1,46 +1,60 @@
 import { Metadata } from "next";
+import Script from "next/script";
 import { CollectionPage } from "@/components/collection-page";
 import { getVideosByCategory } from "@/data/videos";
 import { AmazonProductGrid } from "@/components/amazon-product-grid";
 
 export const metadata: Metadata = {
   title: "Bollywood Movies | Onemovie",
-  description: "Watch the latest Bollywood movies, Hindi films, and Indian cinema. Stream blockbuster hits and classic favorites.",
+  description:
+    "Watch the latest Bollywood movies, Hindi films, and Indian cinema. Stream blockbuster hits and classic favorites.",
 };
 
 export default function BollywoodPage() {
   const videos = getVideosByCategory("Bollywood");
-  
-{/* 300x250 Ad Banner */}
-<div className="flex justify-center my-6">
-  <div
-    dangerouslySetInnerHTML={{
-      __html: `
-        <script>
-          atOptions = {
-            'key' : '6231dfc46d1c0ab675dd42b3bf8fb11c',
-            'format' : 'iframe',
-            'height' : 250,
-            'width' : 300,
-            'params' : {}
-          };
-        </script>
-        <script
-          type="text/javascript"
-          src="//www.profitabledisplaynetwork.com/xxxxx/invoke.js">
-        </script>
-      `,
-    }}
-  />
-</div>
+
   return (
-    <CollectionPage
-      title="Bollywood Movies"
-      description="Explore the best of Hindi cinema - from blockbuster hits to timeless classics"
-      videos={videos}
-      basePath="/bollywood"
-      availableLanguages={["Hindi"]}
-    >
-    </CollectionPage>
+    <>
+      {/* 300x250 Ad Banner */}
+      <div className="flex justify-center my-6">
+        <div
+          id="ad-300x250"
+          style={{
+            width: "300px",
+            height: "250px",
+          }}
+        >
+          <Script
+            id="ad-options-300x250"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                atOptions = {
+                  'key' : '6231dfc46d1c0ab675dd42b3bf8fb11c',
+                  'format' : 'iframe',
+                  'height' : 250,
+                  'width' : 300,
+                  'params' : {}
+                };
+              `,
+            }}
+          />
+
+          <Script
+            id="ad-invoke-300x250"
+            strategy="afterInteractive"
+            src="//www.profitabledisplaynetwork.com/6231dfc46d1c0ab675dd42b3bf8fb11c/invoke.js"
+          />
+        </div>
+      </div>
+
+      <CollectionPage
+        title="Bollywood Movies"
+        description="Explore the best of Hindi cinema - from blockbuster hits to timeless classics"
+        videos={videos}
+        basePath="/bollywood"
+        availableLanguages={["Hindi"]}
+      />
+    </>
   );
 }
