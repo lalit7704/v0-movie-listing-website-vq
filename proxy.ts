47c4f003ac316249c5a732461bdfa1155231c749
Lalit <lalit979932@gmail.com>
2026-09-18 12:26:51 +0530

    poster

--- proxy.ts
+++ proxy.ts
@@ -1,13 +1,7 @@
-import { NextResponse, type NextRequest } from "next/server";
+import { type NextRequest } from "next/server";
 import { updateSession } from "@/lib/supabase/proxy";
 
 export async function proxy(request: NextRequest) {
-  if (request.nextUrl.hostname === "www.onemovie.in") {
-    const url = request.nextUrl.clone();
-    url.protocol = "https:";
-    url.host = "onemovie.in";
-    return NextResponse.redirect(url, 308);
-  }
   return updateSession(request);
 }
 
